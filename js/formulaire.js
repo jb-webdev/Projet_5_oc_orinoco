734b4985f5b28280002d659b04ca7db4c4800df5
j.boero <boero.dw@gmail.com>
2020-08-02 19:11:10 +0200

    validation des formulaires

--- js/formulaire.js
+++ js/formulaire.js
@@ -8,7 +8,7 @@ let ajouterFomrulaire = function () {
     h2Titre.innerHTML = "votre formulaire de commande";
 
     // === on crée nos formulaires 
-    const formulaireInput = document.getElementById("formInput");  // je recupere la balise form
+    const formulaireInput = document.getElementById("inscription");  // je recupere la balise form
     
     const inputLastname = document.createElement('input');
     inputLastname.setAttribute('type', 'text');
@@ -63,6 +63,10 @@ let ajouterFomrulaire = function () {
     inputBtnConfirm.setAttribute('value', 'Valider votre commande');
     inputBtnConfirm.className = "btnInputEnvoyerFormulaire";
     formulaireInput.appendChild(inputBtnConfirm);
+
+    const messageErreur = document.createElement('p');
+    messageErreur.id = "erreur";
+    formulaireInput.appendChild(messageErreur);
 };
 // =========================== fin de la fonction ajout formulaire =======================
 // =======================================================================================
@@ -93,8 +97,9 @@ let supprimerFomrulaire = function () {
     suppInputEmail.remove();
     suppInputEmail2.remove();
     suppInputbutton.remove();
+    document.getElementById("erreur").innerHTML = "";
 
-    // ======== je creer mes nouvelles balise pouir lr message de remerciment
+    // ======== je creer mes nouvelles balise pour le message de remerciment
 
     const paragraphe = document.getElementById("h2Titre");
     h2Titre.innerHTML = "Validation commande";
@@ -173,119 +178,52 @@ let  fonctionEnvoiFormulmaire = function () {
 //=====================================================================================
 // ========= a l'ouverture de la page j'affiche =======================================
 
-// ========== la quantite d'article dans le panier
+// ========== la quantite d'article dans le panier ==============================
 document.querySelector('.panierNav').innerHTML = "Retour";
 
-// ========== les formulaires 
+// ========== les formulaires ========================================
 let affichage = ajouterFomrulaire(); // on affiche le formulaire à l'ouverture de la page
 
-// j'utilise un ecouter d'evenement pour envoyer ma fonction
-document.querySelector(".btnInputEnvoyerFormulaire").addEventListener("click", function(){
+//  ===================================   verification formulaire ========================================
+
+document.querySelector(".btnInputEnvoyerFormulaire").addEventListener("click", function() {
+    let erreur;
+    let verifPrenom = document.getElementById("inputBoxFormPrenom");
+    let verifNom = document.getElementById("inputBoxFormNom");
+    let verifAdresse = document.getElementById("inputBoxFormAdresse");
+    let verifVille = document.getElementById("inputBoxFormVille");
+    let verifEmail = document.getElementById("inputBoxFormEmail");
+    let verifConfirm = document.getElementById("inputBoxFormEmailConfirm");
+// if (verifAdresse.value != verifEmail) {
+//     erreur = "EMAIL DIFFERENT !";
+// }
+if (!verifConfirm.value) {
+    erreur = "VEUILLEZ CONFIRMER VOTRE EMAIL !";
+}
+if (!verifEmail.value) {
+    erreur = "VEUILLEZ RENSEIGNER VOTRE EMAIL !";
+}
+if (!verifVille.value) {
+    erreur = "VEUILLEZ RENSEIGNER VOTRE VILLE !";
+}
+if (!verifAdresse.value) {
+    erreur = "VEUILLEZ RENSEIGNER VOTRE ADRESSE !";
+}
+if (!verifPrenom.value) {
+    erreur = "VEUILLEZ RENSEIGNER VOTRE PRENOM !";
+}
+if (!verifNom.value) {
+    erreur = "VEUILLEZ RENSEIGNER VOTRE NOM !";
+}
+if (erreur) {
+    document.getElementById("erreur").innerHTML = erreur;
+} else {
+    // ==== si tous est bon j'envoie la demande et j'affiche les remerciment et j'efface le localstorage ==============
     fonctionEnvoiFormulmaire();
     supprimerFomrulaire();
     localStorage.clear();
     sessionStorage.clear();
+}
 });
 
-
-
-
-// ====================================================================================================================================
-// ===================================== ca fonctionnne presque SAUVEGARDE AVENT DE CHANGER LES OBJET ET LES ARRAY ====================
-// ====================================================================================================================================
-
-// // ============================= fonction post requet ajax ==================
-// let  fonctionEnvoiFormulmaire = function () {
-//     let name = document.getElementById("inputBoxFormNom").value;
-//     let firstName = document.getElementById("inputBoxFormPrenom").value;
-//     let adressUser = document.getElementById("inputBoxFormAdresse").value;
-//     let cityUser = document.getElementById("inputBoxFormVille").value;
-//     let emailUser = document.getElementById("inputBoxFormEmail").value;
-//     // console.log(firstName); // je controle que je recupere bien la valeur de la variable 
-    
-//     // format objet pour l'utilisateur
-//     let contact = []; // on cree une variable pour nos objets
-//     class infoForm {        // je cree une class pour stocker mes informations utilisateur
-//         constructor (firstName, name, adress, city, email) {
-//             this.firstName = firstName,
-//             this.name = name,
-//             this.adress = adress,
-//             this.city = city,
-//             this. email = email
-//         }
-//     };
-
-//     // format objet pour les articles
-//     let producs = []; // je cree une variable pour les infos des articles
-//     class Producs {        // je cree une class pour stocker mes informations utilisateur
-//         constructor (id, name, price, quantite) {
-//             this.id = id,
-//             this.name = name,
-//             this.price = price,
-//             this.quantite = quantite
-//         }
-//     };
-
-//     //je creer un class pour le format à envoyer à l'api
-//     let formaToSend = []; // je cree mon objet à envoyer
-
-//     class FormatToSend {  // je cree un constructeur
-//         constructor(contact, producs) {
-//             this.contact = contact;
-//             this.producs = producs
-//         }
-//     };
-//     lectureObjet = localStorage.getItem("panier"); // je recupere les informations avec la methode get pour controler la presence de mon objet dans le localStorage
-//     objJson = JSON.parse(lectureObjet); // je parse les infos pour pouvoir les traiter 
-
-//     user = new infoForm (
-//         firstName,
-//         name,
-//         adressUser,
-//         cityUser,
-//         emailUser
-//     )
-//     contact.push(user); // je pousse les informations dans mon objet contact
-    
-//     console.log(contact); // je controle que j'ai bien stocker mes information dans l'objet
-//     // je recupere mes articles pour les stocker dans un objet
-
-//     // j'utilise une boucle for pour recuperer les donnees des articles
-//     for(i = 0 ; i < objJson.length; i++) {
-//         objs = new Producs (
-//         objJson[i].id,
-//         objJson[i].name,
-//         objJson[i].price,
-//         objJson[i].quantite
-//         )
-//         producs.push(objs); // je pousse les informations dans mon objet producs
-//     }
-//     console.log(producs); // je controle que les informations sont bien preésente dans mon objet producs
-
-//     formatObj = new FormatToSend ( 
-//         contact,
-//         producs
-    
-//     )
-//     formaToSend.push(formatObj); // je pousse deux objet (contact et producs) dans mon objet formaToSend
-//     console.log(formaToSend); // je controle que mon objet "formaToSend" contient bien les infos
-
-//     //  ==================== on envoie la confirmation à l'API ===============
-//     const xhr = new XMLHttpRequest();
-
-//     xhr.onreadystatechange = function () {
-//         if (this.readyState == 4 && this.status == 200) {
-//             let response = JSON.parse(this.responseText);
-//         }
-//     };
-//     xhr.open("POST", "http://localhost:3000/api/cameras/order"), true;
-//     xhr.setRequestHeader("Content-Type", "application/json");
-//     xhr.send(JSON.stringify(formaToSend));
-//     }
-    
-    
-
-// // j'utilise un ecouter d'evenement pour envoyer ma fonction
-// document.querySelector(".btnInputEnvoyerFormulaire").addEventListener("click", function(){
-//     fonctionEnvoiFormulmaire();
-// });
+// ======================== fin verification formulaires ==============================
